0ca5a9f5266bdc0b34b27fd9706b35a7448bec71
Renato Sousa <renato.sousa@supplier.com.br>
2019-11-22 15:37:25 -0300

    Ajuste API

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(routes);
 
+app.get('*', res => {
+    res.status(404).send('NOT FOUND');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port);
